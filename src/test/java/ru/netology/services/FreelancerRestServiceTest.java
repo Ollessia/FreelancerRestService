9fea58417e5822ef048ef0c5d534b840ef57abea
Olesia <lisa999lisa@mail.ru>
2023-12-15 22:38:48 +0300

    Initial commit

---
+++ src/test/java/ru/netology/services/FreelancerRestServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FreelancerRestServiceTest {
+    @Test
+    public void testCalculate1 () {
+        FreelancerRestService servise = new FreelancerRestService();
+
+       int actual = servise.calculate(10_000, 3_000, 20_000);
+       int expected = 3;
+
+        Assertions. assertEquals(expected, actual);
+
+
+
+
+    }
+
+
+  @Test
+    public void testCalculate2 () {
+        FreelancerRestService servise = new FreelancerRestService();
+
+        int actual = servise.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    }
